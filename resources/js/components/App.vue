c2f611e3313e8250fbdbe1362f07b7d4fc0469d3
franciowel <spina_francesco@blu.it>
2022-09-29 16:12:19 +0200

    layout vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app">
+    <HeaderComponents/>
+    <CarouselTags />
+    <WorkWithUs/>
+    <FooterCompo />
+    <div class="responsive_my_tb">
+      <ResponsiveTablet />
+    </div>
+    <!-- <div class="responsive_my_sp">
+        <ResponsiveHome />
+    </div> -->
+  </div>
+</template>
+
+<script>
+import HeaderComponents from './components/HeaderComponents.vue';
+import CarouselTags from './components/CarouselTags.vue';
+import WorkWithUs from './components/WorkWithUs.vue';
+import FooterCompo from './components/FooterCompo.vue';
+import ResponsiveTablet from './components/Responsive/ResponsiveTablet.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponents,
+    CarouselTags,
+    WorkWithUs,
+    FooterCompo,
+    ResponsiveTablet,
+  }
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;700&family=Pacifico&display=swap'); 
+
+#app {
+  font-family: 'IBM Plex Sans', sans-serif;
+}
+
+ @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;700&family=Pacifico&display=swap');
+  body{
+    font-family: 'IBM Plex Sans', sans-serif;
+  }
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    img{
+      width: 100%;
+    }
+    a{
+      text-decoration: none;
+      color: inherit;
+    }
+  }
+
+  .carrousel_my{
+      margin-top: 150px;
+      display: flex;
+      justify-content: center;
+     overflow: hidden;
+    }
+    .responsive_my_sp{
+      display: none;
+    }
+    .responsive_my_tb{
+      display: none;
+    }
+    @media screen and (max-width: 920px) {
+      .carrousel_my{
+         display: none;
+    }
+    .responsive_my_tb{
+      display:block;
+    }
+}
+    @media screen and (max-width: 620px) {
+      .responsive_my_tb{
+        display:none;
+      }
+    .carrousel_my{
+      display: none;
+    }
+    .responsive_my_sp{
+      display: block;
+    }
+}
+</style>
---
+++ resources/js/components/MainPages/Section/CarouselComponent.vue
@@ -0,0 +1,148 @@
+<template>
+<div class="slider_my">
+	<div class="slide-track_my">
+		<div class="slide_my" v-for="(carrousel, index) in carrousels" :key="index">
+      <a href="#">
+        <img :src="carrousel.url" height="100" width="250" alt="" />
+      </a>
+		</div>
+
+	</div>
+</div>
+</template>
+/* eslint-disable */
+<script>
+
+	export default {
+		name: 'CarouselComponents',
+    data() {
+      return{
+        carrousels: [
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+          {
+            url: 'https://www.buttalapasta.it/wp-content/uploads/2017/11/pizza-margherita.jpg',
+          },
+       
+        ],
+      }
+    }
+}
+</script>
+/* eslint-disable */  	  
+	
+<style scoped lang="scss">
+ /* eslint-disable */
+ @import "../../assets/sass/variable.scss";
+
+$animationSpeed: 40s;
+
+// Animation
+@keyframes scroll {
+	0% { transform: translateX(0); }
+	100% { transform: translateX(calc(-250px * 7))}
+}
+
+
+// Styling
+.slider_my {	
+	
+	position: relative;
+	
+	
+	&::before,
+	&::after {
+	
+		content: "";
+		height: 100px;
+		position: absolute;
+		width: 200px;
+		z-index: 2;
+	}
+	
+	&::after {
+		right: 0;
+		top: 0;
+		transform: rotateZ(180deg);
+	}
+
+	&::before {
+		left: 0;
+		top: 0;
+	}
+	
+	.slide-track_my {
+		animation: scroll $animationSpeed linear infinite;
+		display: flex;
+		width: calc(350px * 7);
+	}
+	
+	.slide_my {
+	
+    width: calc(100% / 7);
+    margin-left: 10px;
+    display: flex;
+    align-items: center;
+}
+    display: flex;
+    align-items: center;
+    a{
+      display: inline-block;
+      width: 100%;
+    }
+   img{
+    width: 100%;
+    border-radius: 15px;
+    border: 1px solid;
+   }
+	}
+  .slide_my:hover{
+    img{
+    width: 100%;
+    border-radius: 15px;
+    border: 5px solid  $maincolor;
+   }
+  }
+
+
+</style>
