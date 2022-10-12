fa53041ed3acc788a0933644bad5c5b7d76c0b4b
DavidePisani <davide.pisani96@gmail.com>
2022-10-12 10:00:47 +0200

    fine chart.js

--- app/Http/Controllers/Admin/ChartController.php
+++ app/Http/Controllers/Admin/ChartController.php
@@ -14,36 +14,36 @@ class ChartController extends Controller
 
         $user = Auth::user();
         // prendo tuttiglim ordini per mese  
-        $sql_query_monthly_orders = 'SELECT COUNT(id) as orders_per_month, created_at FROM orders  WHERE user_id = ' . $user->id . ' GROUP BY month(created_at) ORDER BY created_at DESC';
+        $monthly_orders = 'SELECT COUNT(id) as orders_month, created_at FROM orders 
+         WHERE user_id = ' . $user->id . '
+         GROUP BY month(created_at) ORDER BY created_at DESC';
         // importo tutte le entrate per mese 
-        $sql_query_monthly_revenue = 'SELECT  SUM(total_price) as revenue_per_month, created_at FROM orders WHERE user_id = '  . $user->id . ' GROUP BY month(created_at) ORDER BY created_at DESC';
+        $monthly_revenue = 'SELECT  SUM(total_price) as revenue_month, created_at FROM orders 
+        WHERE user_id = '  . $user->id . ' 
+        GROUP BY month(created_at) ORDER BY created_at DESC';
 
-        $user_orders = DB::select($sql_query_monthly_orders);
-        $user_revenues = DB::select($sql_query_monthly_revenue);
+        $user_orders = DB::select($monthly_orders);
+        $user_revenues = DB::select($monthly_revenue);
 
-        $monthly_orders = [];
-        $orders_by_month_year = [];
-        $monthly_revenue = [];
+        $orders = [];
+        $time_stamp = [];
+        $revenues = [];
 
         foreach($user_orders as $order) {
-            $monthly_orders[] = $order->orders_per_month;
-            $orders_by_month_year[] = Carbon::create($order->created_at)->format('m/Y');
+            $orders[] = $order->orders_month;
+            $time_stamp[] = Carbon::create($order->created_at)->format('m/Y');
         }
 
         foreach($user_revenues as $revenue) {
-            $monthly_revenue[] = $revenue->revenue_per_month;
+            $revenues[] = $revenue->revenue_month;
         }
 
-        $chart_types = ['bar', 'line'];
-
         $data = [
-            'user' => $user,
-            'chart_types' => $chart_types,
-            'monthly_orders' => $monthly_orders,
-            'monthly_revenue' => $monthly_revenue,
-            'orders_by_month_year' => $orders_by_month_year,
+            'orders' => $orders,
+            'revenues' => $revenues,
+            'time_stamp' => $time_stamp,
         ];
 
-        return view('admin.statistics', $data);
+        return view('admin.chart', $data);
     }
 }
--- resources/views/admin/chart.blade.php
+++ resources/views/admin/chart.blade.php
@@ -5,35 +5,43 @@
     <div>
         <canvas id="myChart"></canvas>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     <script>
-        const labels = [
-          
-        ];
+        const labels = <?php echo json_encode(array_reverse($time_stamp));?>
+        
+        
+        
       
         const data = {
           labels: labels,
-          datasets: [{
-            label: 'Statistiche ordini ricevuti',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
-          }]
+          datasets: [
+            {
+                label: 'Statistiche ordini ricevuti',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: <?php echo json_encode($orders);?>,
+            },
+            {
+                label: 'Statistiche entrate ricevute',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: <?php echo json_encode($revenues);?>,
+            }
+        ]
+
         };
       
         const config = {
-          type: 'line',
+          type: 'bar',
           data: data,
           options: {}
         };
-      </script>
-
-    <script>
+      
         const myChart = new Chart(
         document.getElementById('myChart'),
         config
         );
-  </script>
-      
-
+    </script>
 @endsection
